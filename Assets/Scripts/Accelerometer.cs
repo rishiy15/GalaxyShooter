9100a58b8e336ac4ec188cd54355056fe2f5f121
Hrushikesh (Rishi) Yemme <rishi.yemme@gmail.com>
2016-10-11 12:28:15 -0400

    Adding project files

---
+++ Assets/Scripts/Accelerometer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Accelerometer : MonoBehaviour 
+{
+
+		
+	void Update () 
+	{
+		transform.Translate(Input.acceleration.x, 0, -Input.acceleration.z);
+	} 
+}
---
+++ Assets/Scripts/TouchInput.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchInput : MonoBehaviour {
+
+
+	/* void Update ()  {
+		Touch myTouch = Input.GetTouch(0);
+		
+		Touch[] myTouches = Input.touches;
+		for(int i = 0; i < Input.touchCount; i++)
+		{
+			nextFire = Time.time + fireRate;
+			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+			audio.Play ();
+
+		}
+	}*/
+}
